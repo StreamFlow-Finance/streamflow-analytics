f8dc48e33352b27ada2e69507182706b0ee34ff7
DanWizard <tortortorented@gmail.com>
2022-01-28 20:43:21 +0200

    server attempt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 mod fetch_logic;
+use actix_web::{get, web, App, HttpServer, Responder};
 
-#[tokio::main]
-async fn main() {
-    fetch_logic::fetch().await
+#[get("/")]
+async fn index() -> impl Responder {
+    fetch_logic::fetch().await;
+    format!("Hello")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
